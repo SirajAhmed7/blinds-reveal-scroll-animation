7baa7a3da0bde20a4129d9fedf4c58c185a7ddee
SirajAhmed <sirajahmedkhatri32@gmail.com>
2026-01-30 18:43:45 +0530

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ReactLenis } from "lenis/react";
 
 import Blinds from "./Blinds";
+import Header from "./Header";
 
 function App() {
   return (
     <>
       <ReactLenis root />
       <div className="min-h-screen">
+        <Header heading={"Blinds Reveal Scroll"} githubLink={"#"} />
         <Blinds />
       </div>
     </>
